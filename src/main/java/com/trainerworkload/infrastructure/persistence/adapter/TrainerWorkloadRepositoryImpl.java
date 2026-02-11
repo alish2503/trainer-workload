e902f52d0c486c88b80593d488e05b3ad408ddef
Alish <ali.tomash@mail.ru>
2026-02-11 12:40:42 +0500

    Added stubs for no-integration cases

--- src/main/java/com/trainerworkload/infrastructure/persistence/adapter/TrainerWorkloadRepositoryImpl.java
+++ src/main/java/com/trainerworkload/infrastructure/persistence/adapter/TrainerWorkloadRepositoryImpl.java
@@ -5,11 +5,13 @@ import com.trainerworkload.domain.port.TrainerWorkloadRepository;
 import com.trainerworkload.infrastructure.persistence.mapper.TrainerWorkloadDocumentMapper;
 import com.trainerworkload.infrastructure.persistence.mongorepo.TrainerTrainingSummaryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
+@Profile("integration")
 public class TrainerWorkloadRepositoryImpl implements TrainerWorkloadRepository {
     private final TrainerTrainingSummaryRepository repository;
 
--- src/main/java/com/trainerworkload/infrastructure/persistence/mongorepo/TrainerTrainingSummaryRepository.java
+++ src/main/java/com/trainerworkload/infrastructure/persistence/mongorepo/TrainerTrainingSummaryRepository.java
@@ -1,10 +1,12 @@
 package com.trainerworkload.infrastructure.persistence.mongorepo;
 
 import com.trainerworkload.infrastructure.persistence.document.TrainerTrainingSummaryDocument;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
 
+@Profile("integration")
 public interface TrainerTrainingSummaryRepository extends MongoRepository<TrainerTrainingSummaryDocument, String> {
     Optional<TrainerTrainingSummaryDocument> findByTrainerUsername(String trainerUsername);
 }
